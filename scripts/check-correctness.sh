7bfaaa65bf314978e2c2c4b148611574c10a3dd0
linskybing <linskybing@gmail.com>
2025-04-28 20:46:41 +0800

    update implement version

--- scripts/check-correctness.sh
+++ scripts/check-correctness.sh
@@ -2,8 +2,8 @@
 
 function check(){
     cdo -diffv $1 $2
-    testok=$(cdo -diffv $1 $2 | sed -n '/records differ$/p' | tr -s " " | cut -f2 -d " ")
-   
+    testok=$(cdo -diffv $1 $2 | sed -n '/fields differ$/p' | tr -s " " | cut -f2 -d " ")
+
     if [[ $testok -eq 0 ]] ; then
         echo "[OK] Bit-identical results"
     else
